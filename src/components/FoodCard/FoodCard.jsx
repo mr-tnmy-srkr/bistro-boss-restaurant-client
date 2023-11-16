2d2f83e03367fc217d1d9e17daa85c7192990948
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-16 11:44:31 +0530

    add to cart complete

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,5 +1,56 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from 'axios'
+
 const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
+    const {name, image, price, recipe, _id} = item;
+    const {user} = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleAddToCart = item => {
+        console.log(item);
+        if(user && user.email){
+            const cartItem = {menuItemId: _id, name, image, price, email: user.email}
+         /*    fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(cartItem)
+            }) */
+            axios.post('http://localhost:5000/carts',cartItem)
+     
+            .then(res => {
+                console.log(res.data)
+                if(res.data.insertedId){
+                    // refetch(); // refetch cart to update the number of items in the cart
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${name} added on the cart.`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please login to order the food',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login', {state: {from: location}})
+                }
+              })
+        }}
+     
     return (
         <div className="card bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -8,7 +59,9 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+                    <button
+                  onClick={() => handleAddToCart(item)} 
+                     className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
                 </div>
             </div>
         </div>
