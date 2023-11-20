d9c12d23599e893a9433aa83f22940a14ed2c642
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-11-20 19:02:22 +0530

    payment history page coding done

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -104,7 +104,7 @@ const CheckoutForm = () => {
         showConfirmButton: false,
         timer: 1500,
       });
-      // navigate("/dashboard/paymentHistory");
+      navigate("/dashboard/paymentHistory");
     }
   };
 
