2b29cda4133a88f9249571aa175851590b858c9e
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-12-03 20:01:49 +0530

    project deployed

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -108,7 +108,9 @@ export const router = createBrowserRouter([
           </AdminRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/menu/${params.id}`),
+          fetch(
+            `https://bistro-boss-restaurant-server-henna.vercel.app/menu/${params.id}`
+          ),
       },
       {
         path: "users",
